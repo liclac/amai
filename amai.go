add7627e36e0e60f90bfa90f6cd42d3303f6a03c
uppfinnarn <uppfinnarn@gmail.com>
2016-02-20 17:52:48 +0100

    Old

--- amai.go
+++ amai.go
@@ -50,13 +50,4 @@ func main() {
 	}
 	
 	app.Run(os.Args)
-	
-	/*doc, err := goquery.NewDocument("http://na.finalfantasyxiv.com/lodestone/character/7248246/")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	nameBox := doc.Find(".player_name_txt")
-	name := nameBox.Find("h2 a").Text()
-	fmt.Printf("Name: %s\n", name)*/
 }
