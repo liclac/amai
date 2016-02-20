bca3253d7801643259bfae522c559ecaa15f8940
uppfinnarn <uppfinnarn@gmail.com>
2016-02-20 12:50:26 +0100

    gofmt wants tabs

--- amai.go
+++ amai.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "github.com/PuerkitoBio/goquery"
+	"fmt"
+	"log"
+	"github.com/PuerkitoBio/goquery"
 )
 
 func main() {
-  doc, err := goquery.NewDocument("http://na.finalfantasyxiv.com/lodestone/character/7248246/") 
-  if err != nil {
-    log.Fatal(err)
-  }
-  
-  nameBox := doc.Find(".player_name_txt")
-  name := nameBox.Find("h2 a").Text()
-  fmt.Printf("Name: %s\n", name)
+	doc, err := goquery.NewDocument("http://na.finalfantasyxiv.com/lodestone/character/7248246/")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	nameBox := doc.Find(".player_name_txt")
+	name := nameBox.Find("h2 a").Text()
+	fmt.Printf("Name: %s\n", name)
 }
