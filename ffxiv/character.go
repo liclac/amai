02136fbdc2215b7f27f0c01f289e13f318967a5d
uppfinnarn <uppfinnarn@gmail.com>
2016-02-21 15:13:39 +0100

    Parse Race/Clan/Gender

--- ffxiv/character.go
+++ ffxiv/character.go
@@ -5,4 +5,7 @@ type FFXIVCharacter struct {
 	ID           string `json:"id"`
 	Name         string `json:"name"`
 	ServerName   string `json:"server_name"`
+	RaceName     string `json:"race_name"`
+	ClanName     string `json:"clan_name"`
+	Gender       string `json:"gender"`
 }
--- ffxiv/parsing.go
+++ ffxiv/parsing.go
@@ -12,8 +12,15 @@ func normalizeServerName(name string) string {
 
 func parseCharacter(id string, doc *goquery.Document) (char FFXIVCharacter, err error) {
 	char = FFXIVCharacter{}
+	
 	char.ID = id
 	char.Name = doc.Find(".player_name_txt h2 a").Text()
 	char.ServerName = normalizeServerName(doc.Find(".player_name_txt h2 span").Text())
+	
+	infoParts := strings.Split(doc.Find(".chara_profile_title").Text(), "/")
+	char.RaceName = strings.TrimSpace(infoParts[0])
+	char.ClanName = strings.TrimSpace(infoParts[1])
+	char.Gender = strings.TrimSpace(infoParts[2])
+	
 	return char, nil
 }
