a87619dae9b4d68d6d8ae3dec8baefe6def74e79
uppfinnarn <uppfinnarn@gmail.com>
2016-02-21 15:02:44 +0100

    Nope, the desktop site is easier to parse

--- ffxiv/adapter.go
+++ ffxiv/adapter.go
@@ -16,7 +16,6 @@ func NewAdapter() *FFXIVAdapter {
 	return &FFXIVAdapter{
 		*base.NewAdapter(map[string]string {
 			"Cookie": "ldst_touchstone=1;ldst_is_support_browser=1;ldst_visit=1",
-			"User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1",
 		}),
 	}
 }
