a9888e3227580740408ae76d7ac23e7e52cb782c
uppfinnarn <uppfinnarn@gmail.com>
2016-02-21 15:07:36 +0100

    Selectors for the desktop site

--- ffxiv/parsing.go
+++ ffxiv/parsing.go
@@ -6,13 +6,14 @@ import (
 )
 
 func normalizeServerName(name string) string {
+	name = strings.TrimSpace(name)
 	return strings.TrimSuffix(strings.TrimPrefix(name, "("), ")")
 }
 
 func parseCharacter(id string, doc *goquery.Document) (char FFXIVCharacter, err error) {
 	char = FFXIVCharacter{}
 	char.ID = id
-	char.Name = doc.Find(".txt_charaname").Text()
-	char.ServerName = normalizeServerName(doc.Find(".txt_worldname").Text())
+	char.Name = doc.Find(".player_name_txt h2 a").Text()
+	char.ServerName = normalizeServerName(doc.Find(".player_name_txt h2 span").Text())
 	return char, nil
 }
