024ce3195f740328b8716f4b31a1c9748a09756b
uppfinnarn <uppfinnarn@gmail.com>
2016-02-22 04:20:31 +0100

    Local map ruins everything (formatting)

--- ffxiv/character.go
+++ ffxiv/character.go
@@ -2,23 +2,23 @@ package ffxiv
 
 // A FFXIV character.
 type FFXIVCharacter struct {
-	ID           uint64 `json:"id"`
-	Name         string `json:"name"`
-	Title        string `json:"title"`
-	ServerName   string `json:"server_name"`
-	Race         string `json:"race"`
-	Clan         string `json:"clan"`
-	Gender       string `json:"gender"`
-	BirthDay     int    `json:"birth_day"`
-	BirthMonth   int    `json:"birth_month"`
-	Guardian     string `json:"guardian"`
+	ID           uint64         `json:"id"`
+	Name         string         `json:"name"`
+	Title        string         `json:"title"`
+	ServerName   string         `json:"server_name"`
+	Race         string         `json:"race"`
+	Clan         string         `json:"clan"`
+	Gender       string         `json:"gender"`
+	BirthDay     int            `json:"birth_day"`
+	BirthMonth   int            `json:"birth_month"`
+	Guardian     string         `json:"guardian"`
 	GrandCompany struct {
-		Name     string `json:"name"`
-		Rank     int    `json:"rank"`
-	}                   `json:"grand_company"`
+		Name     string         `json:"name"`
+		Rank     int            `json:"rank"`
+	}                           `json:"grand_company"`
 	FreeCompany  struct {
-		ID       uint64 `json:"id"`
-		Name     string `json:"name"`
-	}                   `json:"free_company"`
+		ID       uint64         `json:"id"`
+		Name     string         `json:"name"`
+	}                           `json:"free_company"`
 	Stats        map[string]int `json:"stats"`
 }
